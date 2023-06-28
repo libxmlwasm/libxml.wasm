f7416511e22d3656e0eec5cf64a843f6e626f556
Cabbage Lettuce <33714346+pycabbage@users.noreply.github.com>
2023-06-28 22:10:17 +0900

    fix workflow

--- scripts/vendor/20-libxml2.sh
+++ scripts/vendor/20-libxml2.sh
@@ -12,7 +12,7 @@ PREFIX=${PREFIX:-$(realpath "./prefix")}
 
 if [ ! -d "$DIRPATH" ]; then
   if [ ! -f "$FILEPATH" ]; then
-    mkdir -p "$TMPDIR"
+    mkdir -p $(dirname "$FILEPATH")
     curl -kLo "$FILEPATH" "$URL"
   else
     echo "File $FILEPATH already exists."
