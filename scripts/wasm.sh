c386fd3a5928b2f03970b7cdc3f3910b7aeb6166
Cabbage Lettuce <33714346+pycabbage@users.noreply.github.com>
2024-05-09 16:58:00 +0900

    update build

--- scripts/wasm.sh
+++ scripts/wasm.sh
@@ -3,7 +3,7 @@
 BASEDIR=${BASEDIR:-$(realpath .)}
 BUILDDIR=${BASEDIR}/wasm-build
 INSTALLPATH=${INSTALLPATH:-$(realpath ${BASEDIR}/dist)}
-NVM_DIR="$HOME/.nvm"
+NVM_DIR=${NVM_DIR:-"$HOME/.nvm"}
 . ~/.nvm/nvm.sh
 
 echo BASEDIR: ${BASEDIR}
@@ -12,8 +12,32 @@ echo INSTALLPATH: ${INSTALLPATH}
 
 sudo chmod 777 . node_modules .pnpm-store
 PATH=$PATH:$HOME/.pnpm-global/bin:/src/node_modules/.bin
-GITHUB_TOKEN=_DUMMY_ corepack pnpm install
+GITHUB_TOKEN=${GITHUB_TOKEN:-"_DUMMY_"} corepack pnpm install
 
-emcmake cmake -GNinja -S ${BASEDIR}/wasm -B ${BUILDDIR}/ -DCMAKE_INSTALL_PREFIX="${INSTALLPATH}"
-cmake --build ${BUILDDIR}/ -j$(nproc)
-cmake --install ${BUILDDIR}/
+build() {
+  local SUFFIX=${1:-".js"}
+  if [ "${SUFFIX}" = ".js" ]; then
+    TYPE="cjs"
+  elif [ "${SUFFIX}" = ".mjs" ]; then
+    TYPE="esm"
+  else
+    TYPE="other"
+  fi
+  local BUILDDIR_TYPE="${BUILDDIR}/${TYPE}/"
+  local INSTALLPATH_TYPE="${INSTALLPATH}/${TYPE}"
+
+  emcmake cmake -GNinja \
+    -S ${BASEDIR}/wasm \
+    -B ${BUILDDIR_TYPE} \
+    -DCMAKE_INSTALL_PREFIX="${INSTALLPATH_TYPE}" \
+    -DTARGET_SUFFIX="${SUFFIX}"
+  cmake --build ${BUILDDIR_TYPE} -j$(nproc)
+  cmake --install ${BUILDDIR_TYPE}
+}
+
+build .js
+build .mjs
+
+mv ${INSTALLPATH}/cjs/* "${INSTALLPATH}/"
+mv ${INSTALLPATH}/esm/*.mjs "${INSTALLPATH}/"
+rm -fr "${INSTALLPATH}/cjs" "${INSTALLPATH}/esm"
